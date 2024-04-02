fafb51531fbcd2cc4c0f17b4e6a5ae72b428c085
elfbrtc <elif.barutcu.5353@hotmail.com>
2024-04-02 16:13:02 +0300

    -complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react'
-import { Link, useLocation, useNavigate} from 'react-router-dom'
+import { Link, useNavigate} from 'react-router-dom'
 
 
 export default function SignUp() {
