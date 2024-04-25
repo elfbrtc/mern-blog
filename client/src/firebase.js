693bc33e22a997f8cc2a2d76cd697d0e6bc24b4c
elfbrtc <elif.barutcu.5353@hotmail.com>
2024-04-25 11:38:35 +0300

    create project

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXks4zRWe3rnz1Vis6mnfusWYNh4vw8Rw",
+  authDomain: "mern-blog-f5a30.firebaseapp.com",
+  projectId: "mern-blog-f5a30",
+  storageBucket: "mern-blog-f5a30.appspot.com",
+  messagingSenderId: "970129163008",
+  appId: "1:970129163008:web:f8cf6d7055cd905adb69e5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
